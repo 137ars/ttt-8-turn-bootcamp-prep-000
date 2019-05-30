e2777db3a9048f88de198dc8a4b89019c4e4c46a
Alec Simpson <alecrsimpson@gmail.com>
2019-05-30 09:25:53 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -13,6 +13,31 @@ def input_to_index(user_input)
 end
 
 
+def valid_move?(the_board, the_index)
+  
+  def position_taken?(board, index)
+    if board[index] == " " || board[index] == "" || board[index] == nil
+      return false
+    else
+      return true
+    end
+  end
+  
+  def valid_position?(number) 
+    if number.between?(0, 8)
+      return true
+    else
+      return false
+    end
+  end
+  
+  if (position_taken?(the_board, the_index)) == false && (valid_position?(the_index)) == true
+    return true
+  else
+    return false
+  end
+  
+end
 
 def move (array, index, value = "X")
   array[index] = value
