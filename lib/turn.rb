7720fd1272946c9ae04026803c13153945a9d57e
Alec Simpson <alecrsimpson@gmail.com>
2019-05-30 09:46:40 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -30,7 +30,7 @@ def valid_move?(board, index)
     end
   end
   
-  if (position_taken?(board, ind)) == false && (valid_position?(number)) == true)
+  if ((position_taken?(board, ind)) == false && (valid_position?(number)) == true)
     return true
   else
     return false
