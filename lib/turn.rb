1757eedb0132d67c1b835b1e1c4516384c3c3365
Alec Simpson <alecrsimpson@gmail.com>
2019-05-30 09:25:17 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,54 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  index = user_input.to_i
+  index = index - 1
+  return index
+end
+
+def valid_move?(the_board, the_index)
+  def position_taken?(board, index)
+    if board[index] == " " || board[index] == "" || board[index] == nil
+      return false
+    else
+      return true
+    end
+  end
+    
+  def valid_position?(number)
+    if number.between?(0, 8)
+      return true
+    else
+      return false
+    end
+  end
+  
+  if (position_taken?(the_board, the_index)) == false && (input_to_index(user_input)) == true
+      return true
+    else
+      return false
+  end
+end
+
+def move (array, index, value = "X")
+  array[index] = value
+  return array
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  number = gets.strip
+  index = input_to_index(number)
+  if valid_move?(the_board, the_index) == true
+    move (array, index, value = "x")
+    display(board)
+  else
+    turn(board)
+  end
+end
